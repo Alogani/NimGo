f3f7cbdaae800033575e56c50373042c04a6afe3
alogani <alogani@mailfence.com>
2024-06-08 09:53:27 +0200

    Update the docs

--- htmldocs/nimgo.nim
+++ htmldocs/nimgo.nim
@@ -3,6 +3,6 @@ import ../src/nimgo/coroutines
 import ../src/nimgo/eventdispatcher
 import ../src/nimgo/gofile
 import ../src/nimgo/gonet
-import ../src/nimgo/goproc
 import ../src/nimgo/gostreams
+import ../src/nimgo/gotaskscomplete
 import ../src/nimgo/public/gotasks
