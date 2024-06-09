7b95c5e424a52564170b9ca51f3616c0c5e299ff
alogani <alogani@mailfence.com>
2024-06-09 13:55:48 +0200

    Update readme. Makes goStdin/goStdout/goStderr thread vars

--- src/nimgo.nim
+++ src/nimgo.nim
@@ -1,7 +1,7 @@
 import ./nimgo/[eventdispatcher]
 
-# from eventdispatcher
-export insideNewEventLoop, withEventLoop, runEventLoop, sleepAsync
+# from eventdispatcher, which are the only functions useful for normal users
+export sleepAsync, insideNewEventLoop, withEventLoop, runEventLoop, newDispatcher, setCurrentThreadDispatcher
 
 import ./nimgo/public/gotasks
 export gotasks
--- src/nimgo/private/minicoro.c
+++ src/nimgo/private/minicoro.c
@@ -3,9 +3,9 @@
 // Some other options are defined directly on command line:
 //-DMCO_NO_DEBUG
 
-#define MCO_MIN_STACK_SIZE 4096 // We will provide it explicitly
+#define MCO_MIN_STACK_SIZE 8192 // We will provide it explicitly
 #define MCO_DEFAULT_STORAGE_SIZE 0 // We won't use premade push and pop
-#define MCO_DEFAULT_STACK_SIZE 4096
+#define MCO_DEFAULT_STACK_SIZE 8192
 #define MCO_NO_DEFAULT_ALLOCATOR // We will use our own in coroutinememory.nim
 #define MINICORO_IMPL // Important minicoro.h symbols will be private
 #include "./minicoro.h"
