3ac690aa5049211f793cdc5a715f738fcaea9f3d
alogani <alogani@mailfence.com>
2024-06-14 07:57:45 +0200

    Update tests according to windows features

--- tests/tgonet.nim
+++ tests/tgonet.nim
@@ -2,48 +2,48 @@ when defined(windows):
     stderr.write("The features of these files hav enot yet been implemented under windows.\n")
     stderr.write("Skipping the test...\n")
     stderr.flushFile()
-    quit(0)
+else:
 
-import nimgo, nimgo/gonet
+    import nimgo, nimgo/gonet
 
-import std/unittest
+    import std/unittest
 
 
-template ProducerConsumerCode(UseBuffer: bool) =
-    ## Careful, sometime templates mess with tests
-    const ServerPort = Port(12345)
-    const ServerAddr = "127.0.0.1"
+    template ProducerConsumerCode(UseBuffer: bool) =
+        ## Careful, sometime templates mess with tests
+        const ServerPort = Port(12345)
+        const ServerAddr = "127.0.0.1"
 
-    proc consumer() =
-        let client = newGoSocket(buffered = UseBuffer)
-        client.connect(ServerAddr, ServerPort)
-        check client.send("Hello\n") == 6
-        check client.recvLine() == "How are you ?"
-        check client.send("Fine\n") > 0
-        client.close()
+        proc consumer() =
+            let client = newGoSocket(buffered = UseBuffer)
+            client.connect(ServerAddr, ServerPort)
+            check client.send("Hello\n") == 6
+            check client.recvLine() == "How are you ?"
+            check client.send("Fine\n") > 0
+            client.close()
 
-    ## Server code
-    proc processClient(client: GoSocket) =
-        check client.recv(10) == "Hello\n"
-        check client.send("How are you ?\n") > 0
-        check client.recv(10) == "Fine\n"
-        check client.recv(10) == ""
-        client.close()
+        ## Server code
+        proc processClient(client: GoSocket) =
+            check client.recv(10) == "Hello\n"
+            check client.send("How are you ?\n") > 0
+            check client.recv(10) == "Fine\n"
+            check client.recv(10) == ""
+            client.close()
 
-    proc producer() =
-        let server = newGoSocket(buffered = UseBuffer)
-        server.setSockOpt(OptReuseAddr, true)
-        server.bindAddr(ServerPort)
-        server.listen()
-        processClient(server.accept())
-        server.close()
+        proc producer() =
+            let server = newGoSocket(buffered = UseBuffer)
+            server.setSockOpt(OptReuseAddr, true)
+            server.bindAddr(ServerPort)
+            server.listen()
+            processClient(server.accept())
+            server.close()
 
-    withEventLoop():
-        goAsync consumer()
-        goAsync producer()
+        withEventLoop():
+            goAsync consumer()
+            goAsync producer()
 
-test "Producer/consumer - unbuffered socket":
-    ProducerConsumerCode(false)
+    test "Producer/consumer - unbuffered socket":
+        ProducerConsumerCode(false)
 
-test "Producer/consumer - buffered socket":
-    ProducerConsumerCode(true)
+    test "Producer/consumer - buffered socket":
+        ProducerConsumerCode(true)
