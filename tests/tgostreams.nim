478b0f19357d53b5ed35beeb564f066210980154
alogani <alogani@mailfence.com>
2024-06-14 07:52:30 +0200

    Update tests according to windows features

--- tests/tgostreams.nim
+++ tests/tgostreams.nim
@@ -2,52 +2,52 @@ when defined(windows):
     stderr.write("The features of these files hav enot yet been implemented under windows.\n")
     stderr.write("Skipping the test...\n")
     stderr.flushFile()
-    quit(0)
+else:
 
-import nimgo, nimgo/[gofile, gostreams]
+    import nimgo, nimgo/[gofile, gostreams]
 
-import std/unittest
+    import std/unittest
 
 
-test "Producer/consumer":
-    var s = newGoBufferStream()
-    proc producer() =
-        for i in 0..10:
-            sleepAsync(10)
-            s.write("data" & $i)
-        s.close()
+    test "Producer/consumer":
+        var s = newGoBufferStream()
+        proc producer() =
+            for i in 0..10:
+                sleepAsync(10)
+                s.write("data" & $i)
+            s.close()
 
-    proc consumer() =
-        for i in 0..10:
-            check s.readChunk() == "data" & $i
-        check s.readChunk() == ""
+        proc consumer() =
+            for i in 0..10:
+                check s.readChunk() == "data" & $i
+            check s.readChunk() == ""
 
-    withEventLoop:
-        goAsync producer()
-        goAsync consumer()
+        withEventLoop:
+            goAsync producer()
+            goAsync consumer()
 
-test "Timeout":
-    var s = newGoBufferStream()
-    check (goAndWait s.readChunk(10)) == ""
+    test "Timeout":
+        var s = newGoBufferStream()
+        check (goAndWait s.readChunk(10)) == ""
 
-test "Producer/consumer with GoFileStream on top of pipes":
-    var pipes = createGoPipe(buffered = true)
-    var reader = newGoFileStream(pipes.reader)
-    let writer = newGoFileStream(pipes.writer)
-    proc producer() =
-        for i in 0..10:
-            sleepAsync(10)
-            writer.write("data" & $i)
-        writer.close()
+    test "Producer/consumer with GoFileStream on top of pipes":
+        var pipes = createGoPipe(buffered = true)
+        var reader = newGoFileStream(pipes.reader)
+        let writer = newGoFileStream(pipes.writer)
+        proc producer() =
+            for i in 0..10:
+                sleepAsync(10)
+                writer.write("data" & $i)
+            writer.close()
 
-    proc consumer() =
-        for i in 0..10:
-            check reader.readChunk() == "data" & $i
-        check reader.readChunk() == ""
-        reader.close()
+        proc consumer() =
+            for i in 0..10:
+                check reader.readChunk() == "data" & $i
+            check reader.readChunk() == ""
+            reader.close()
 
-    withEventLoop:
-        goAsync producer()
-        goAsync consumer()
-    check reader.closed()
-    check writer.closed()
+        withEventLoop:
+            goAsync producer()
+            goAsync consumer()
+        check reader.closed()
+        check writer.closed()
--- tests/tgotasks.nim
+++ tests/tgotasks.nim
@@ -2,151 +2,151 @@ when defined(windows):
     stderr.write("The features of these files hav enot yet been implemented under windows.\n")
     stderr.write("Skipping the test...\n")
     stderr.flushFile()
-    quit(0)
+else:
 
-import nimgo, nimgo/[coroutines, eventdispatcher]
-import std/unittest
-
-
-test "outside coroutine":
-    withEventLoop():
-        proc coroFn(): int =
-            return 42
-
-        check wait(goasync(coroFn())) == 42
+    import nimgo, nimgo/[coroutines, eventdispatcher]
+    import std/unittest
 
 
-test "inside coroutine":
-    withEventLoop():
+    test "outside coroutine":
+        withEventLoop():
+            proc coroFn(): int =
+                return 42
+
+            check wait(goasync(coroFn())) == 42
+
+
+    test "inside coroutine":
+        withEventLoop():
+            proc innerCoroFn(): int =
+                return 42
+
+            proc coroFn() =
+                check wait(goasync(innerCoroFn())) == 42
+
+            goasync coroFn()
+
+    test "outside coroutine with timeout":
+        withEventLoop():
+            proc coroFn(): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), 200, true)
+                suspend(coro)
+                return 42
+
+            check wait(
+                goasync(coroFn()),
+                100
+                ).isNone()
+
+    test "inside coroutine with timeout":
+        withEventLoop():
+            proc innerCoroFn(): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), 200, true)
+                suspend(coro)
+                return 42
+
+            proc coroFn() =
+                check wait(goasync(innerCoroFn()), 100).isNone()
+
+            var task = goasync coroFn()
+            wait(task)
+            check task.finished()
+
+
+    test "inside coroutine with nested timeout":
         proc innerCoroFn(): int =
+            var coro = getCurrentCoroutine()
+            resumeOnTimer(coro.toOneShot(), 200, true)
+            suspend(coro)
             return 42
 
         proc coroFn() =
             check wait(goasync(innerCoroFn())) == 42
 
-        goasync coroFn()
-
-test "outside coroutine with timeout":
-    withEventLoop():
-        proc coroFn(): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), 200, true)
-            suspend(coro)
-            return 42
-
         check wait(
             goasync(coroFn()),
             100
-            ).isNone()
+            ) == false
 
-test "inside coroutine with timeout":
-    withEventLoop():
-        proc innerCoroFn(): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), 200, true)
-            suspend(coro)
-            return 42
+    test "inside coroutine waitall - success":
+        withEventLoop():
+            proc innerCoroFn(): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), 200, true)
+                suspend(coro)
+                return 42
 
-        proc coroFn() =
-            check wait(goasync(innerCoroFn()), 100).isNone()
+            proc coroFn() =
+                check waitall(
+                    @[
+                        goasync(innerCoroFn()),
+                        goasync(innerCoroFn())
+                    ],
+                    300) == @[42, 42]
 
-        var task = goasync coroFn()
-        wait(task)
-        check task.finished()
+            var task = goasync coroFn()
+            wait(task)
+            check task.finished()
 
+    test "inside coroutine waitall - fail":
+        withEventLoop():
+            proc innerCoroFn(timeoutMs: int): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), timeoutMs, true)
+                suspend(coro)
+                return 42
 
-test "inside coroutine with nested timeout":
-    proc innerCoroFn(): int =
-        var coro = getCurrentCoroutine()
-        resumeOnTimer(coro.toOneShot(), 200, true)
-        suspend(coro)
-        return 42
+            proc coroFn() =
+                check waitall(
+                    @[
+                        goasync(innerCoroFn(100)),
+                        goasync(innerCoroFn(500))
+                    ],
+                    300).len() == 0
 
-    proc coroFn() =
-        check wait(goasync(innerCoroFn())) == 42
+            var task = goasync coroFn()
+            wait(task)
+            check task.finished()
 
-    check wait(
-        goasync(coroFn()),
-        100
-        ) == false
+    test "inside coroutine waitany - fail":
+        withEventLoop():
+            proc innerCoroFn(timeoutMs: int): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), timeoutMs, true)
+                suspend(coro)
+                return 42
 
-test "inside coroutine waitall - success":
-    withEventLoop():
-        proc innerCoroFn(): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), 200, true)
-            suspend(coro)
-            return 42
+            proc coroFn() =
+                check waitAny(
+                    @[
+                        goasync(innerCoroFn(400)),
+                        goasync(innerCoroFn(500))
+                    ],
+                    300) == false
 
-        proc coroFn() =
-            check waitall(
-                @[
-                    goasync(innerCoroFn()),
-                    goasync(innerCoroFn())
-                ],
-                300) == @[42, 42]
+            var task = goasync coroFn()
+            wait(task)
+            check task.finished()
 
-        var task = goasync coroFn()
-        wait(task)
-        check task.finished()
+    test "inside coroutine waitany - success":
+        withEventLoop():
+            proc innerCoroFn(timeoutMs: int): int =
+                var coro = getCurrentCoroutine()
+                resumeOnTimer(coro.toOneShot(), timeoutMs, true)
+                suspend(coro)
+                return 42
 
-test "inside coroutine waitall - fail":
-    withEventLoop():
-        proc innerCoroFn(timeoutMs: int): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), timeoutMs, true)
-            suspend(coro)
-            return 42
+            proc coroFn() =
+                check waitAny(
+                    @[
+                        goasync(innerCoroFn(100)),
+                        goasync(innerCoroFn(500))
+                    ],
+                    300)
 
-        proc coroFn() =
-            check waitall(
-                @[
-                    goasync(innerCoroFn(100)),
-                    goasync(innerCoroFn(500))
-                ],
-                300).len() == 0
-
-        var task = goasync coroFn()
-        wait(task)
-        check task.finished()
-
-test "inside coroutine waitany - fail":
-    withEventLoop():
-        proc innerCoroFn(timeoutMs: int): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), timeoutMs, true)
-            suspend(coro)
-            return 42
-
-        proc coroFn() =
-            check waitAny(
-                @[
-                    goasync(innerCoroFn(400)),
-                    goasync(innerCoroFn(500))
-                ],
-                300) == false
-
-        var task = goasync coroFn()
-        wait(task)
-        check task.finished()
-
-test "inside coroutine waitany - success":
-    withEventLoop():
-        proc innerCoroFn(timeoutMs: int): int =
-            var coro = getCurrentCoroutine()
-            resumeOnTimer(coro.toOneShot(), timeoutMs, true)
-            suspend(coro)
-            return 42
-
-        proc coroFn() =
-            check waitAny(
-                @[
-                    goasync(innerCoroFn(100)),
-                    goasync(innerCoroFn(500))
-                ],
-                300)
-
-        var task = goasync coroFn()
-        wait(task)
-        check task.finished()
-    
+            var task = goasync coroFn()
+            wait(task)
+            check task.finished()
+        
